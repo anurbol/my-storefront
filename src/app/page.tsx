0f59a3ad759421390232b080b0f5ef95057c160f
Nurbol Alpysbayev <anurbol@gmail.com>
2025-10-07 23:05:47 +0500

    Final-ish

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,40 @@
+import { Card, CardContent } from "@/components/ui/card";
+import { API_HOST } from "@/lib/api";
+import { Product } from "@/types/product";
 import Image from "next/image";
+import Link from "next/link";
 
-export default function Home() {
+export default async function HomePage() {
+  const products: Product[] = await getProducts();
+
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <main>
+      <h1 className="text-3xl font-semibold m-5">My Storefront</h1>
+      {products.length === 0 ? (
+        <div>No products found.</div>
+      ) : (
+        <div className="m-5 grid gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+          {products.map((p) => (
+            <Link key={p.id} href={`/products/${p.id}`}>
+              <Card>
+                <CardContent className="p-4 flex flex-col items-center">
+                  <Image src={p.image} alt={p.title} width={200} height={200}/>
+                  <h2 className="text-center font-medium">{p.title}</h2>
+                  <div>${p.price}</div>
+                </CardContent>
+              </Card>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            </Link>
+            
+          ))}
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+      )}
+      
+    </main>
+  )
+}
+
+async function getProducts() {
+  const url = `${API_HOST}/products`
+  return (await fetch(url)).json()
 }
---
+++ src/app/products/[id]/page.tsx
@@ -0,0 +1,58 @@
+import { API_HOST } from "@/lib/api";
+import { Product } from "@/types/product";
+import { PageProps } from "@/types/common";
+import Image from "next/image";
+import { notFound } from "next/navigation";
+
+export default async function ProductPage({params}: PageProps<Params>) {
+  params = await params
+
+  const product: Product | null = await getProduct(params.id)
+  if (!product) return notFound();
+
+  return (
+    <div className="max-w-4xl mx-auto p-5">
+      <div className="flex flex-col sm:flex-row gap-6">
+        <div className="flex-1">
+          <Image src={product.image} alt={product.title} width={600} height={600}>
+          </Image>
+        </div>
+        <div className="flex-2">
+          <h1 className="text-2xl font-bold mb-2">{product.title}</h1>
+          <p className="text-gray-600 mb-4">{product.description}</p>
+          <div className="text-lg font-semibold mb-2">${product.price}</div>
+          <button className="bg-black text-white px-4 py-2 rounded-md">
+            Add to cart
+          </button>
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
+export async function generateMetadata({params}: PageProps<Params>) {
+  params = await params
+  
+  const { title, description, image } = await getProduct(params.id);
+  const ogImage = `/api/og?title=${encodeURIComponent(title)}&image=${encodeURIComponent(image)}`;
+
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      images: [{ url: ogImage }],
+    },
+  };
+}
+
+async function getProduct(id: number) {
+  const url = `${API_HOST}/product/${id}`
+  return (await fetch(url)).json()
+}
+
+interface Params {
+  id: number
+}
